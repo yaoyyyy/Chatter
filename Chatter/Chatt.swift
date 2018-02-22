73e074ff17c489b9fcc7697d9719b9e4594680f3
Yuan Yao <yaoyyyy@umich.edu>
2018-02-21 21:02:03 -0500

    first

---
+++ Chatter/Chatt.swift
@@ -0,0 +1,9 @@
+//
+//  Chatt.swift
+//  Chatter
+//
+//  Created by YY on 1/24/18.
+//  Copyright © 2018 Team Zero. All rights reserved.
+//
+
+import Foundation
---
+++ Chatter/ChattTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  ChattTableCell.swift
+//  Chatter
+//
+//  Created by YY on 1/24/18.
+//  Copyright © 2018 Team Zero. All rights reserved.
+//
+
+import Foundation
